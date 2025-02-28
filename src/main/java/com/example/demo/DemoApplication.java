cb8c6ef3342d4af97d289e5cb7c9eca6db81d7fe
paoloe <paoloespiritu@outlook.com>
2025-02-28 08:51:47 +0000

    student class GET api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import student.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Paolo",
+						"paoloespiritu@outlook.com",
+						21,
+						LocalDate.of(1998, Month.AUGUST,2)
+				)
+		);
 	}
 
 }
