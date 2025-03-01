9da1c1a25d68a38c422edcc4bbd1b743b2fccff8
paoloe <paoloespiritu@outlook.com>
2025-03-01 16:37:13 +0000

    dependency injection - service bean

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,11 @@ import java.util.List;
 @RequestMapping(path ="api/v1/student")
 public class StudentController {
 
+    //reference to student service
     private final StudentService studentService;
 
+    //automatically instantiating StudentService
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
