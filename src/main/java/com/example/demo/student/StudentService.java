40126993fe8088aec5d9df692e600f61760b0cb3
paoloe <paoloespiritu@outlook.com>
2025-03-15 13:56:32 +0000

    functionality to update a record using PUT using @Transactional - need to understand what this is doing further

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,29 @@ public class StudentService {
             throw new IllegalStateException("Student with id " + studentId + " does not exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    //PUT - update an existing record
+    @Transactional // this annotation puts the entity into a managed state
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist"));
+
+        // validate name is not blank, length is > 0 and name values are not the same
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("Student email already exists");
+            }
+            // set the email after all the validation
+            student.setEmail(email);
+        }
     }
 }
